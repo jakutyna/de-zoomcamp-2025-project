4bf1e47a49ee5d1f83295634eb1fedd59ad7f674
Jacek Kutyna <jakutyna@gmail.com>
2025-03-31 22:51:35 +0200

    Add Airflow schedule

--- airflow/dags/met_museum_dag.py
+++ airflow/dags/met_museum_dag.py
@@ -6,7 +6,7 @@ from airflow.decorators import dag, task
 
 
 @dag(
-    schedule=None,
+    schedule="@daily",
     start_date=pendulum.datetime(2025, 1, 1, tz="UTC"),
     catchup=False,
     tags=["met_data"],
